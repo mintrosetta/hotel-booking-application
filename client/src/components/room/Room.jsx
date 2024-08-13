dba31020615e504cf920024e544f9855814f8527
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-08-13 12:11:41 +0700

    add room component

---
+++ client/src/components/room/Room.jsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react"
+import { getAllRooms } from "../utils/ApiFunction";
+import RoomCard from "./RoomCard";
+import { Col, Container, Row } from "react-bootstrap";
+import RoomFilter from "../common/RoomFilter";
+import RoomPaginator from "../common/RoomPaginator";
+
+export default function Room() {
+    const [data, setData] = useState([]);
+    const [errorMsg, setErrorMsg] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [roomPerPage] = useState(6);
+    const [filteredData, setFilteredData] = useState([]);
+
+    useEffect(() => {
+        setIsLoading(true);
+        getAllRooms().then((data) => {
+            setData(data);
+            setFilteredData(data);
+        }).catch((ex) => {
+            setErrorMsg(ex.message);
+        }).finally(() => {
+            setIsLoading(false);
+        });
+
+    }, []);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    }
+
+    const totalPages = Math.ceil(filteredData.length / roomPerPage);
+    const renderRooms = () => {
+        const startIndex = (currentPage - 1) * roomPerPage;
+        const endIndex = (startIndex * roomPerPage);
+
+        return filteredData.slice(startIndex, endIndex).map((room) => <RoomCard key={room.id} room={room} />);
+    }
+
+    if (isLoading) {
+        return (
+            <div>
+                Loading rooms
+            </div>
+        )
+    }
+
+    if (errorMsg !== "") {
+        return <div className="text-danger">{errorMsg}</div>
+    }
+
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col md={6} className="mb-3 mb-md-0">
+                        <RoomFilter data={data} setFilteredData={setFilteredData} />
+                    </Col>
+
+                    <Col md={6} className="d-flex align-items-center justify-content-end">
+                        <RoomPaginator currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+                    </Col>
+                </Row>
+
+                <Row>
+                    {renderRooms()}
+                </Row>
+
+                <Row>
+                    <Col md={6} className="d-flex align-items-center justify-content-end">
+                        <RoomPaginator currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
---
+++ client/src/components/room/RoomCard.jsx
@@ -0,0 +1,28 @@
+import { Card, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function RoomCard({room}) {
+    return (
+        <Col key={room.id} className="mb-4" xs={12}>
+            <Card>
+                <Card.Body className="d-flex flex-wrap align-items-center">
+                    <div className="flex-shrink-0 mr-3 mt-3 mb-md-0">
+                        <Card.Img 
+                            variant="top" 
+                            src={'data:image/png; base64,' + room.photo} 
+                            alt="Room photo" 
+                            style={{ width: "100%", maxWidth: "200px", height: "auto"}} />
+                    </div>
+                    <div className="flex-grow-1 ml-3 px-5">
+                        <Card.Title className="hotel-column">{room.roomType}</Card.Title>
+                        <Card.Title className="room-price">{room.price}</Card.Title>
+                        <Card.Text className="">Some room information goes here the guest to read throungh</Card.Text>
+                    </div>
+                    <div className="flex-shrink-0 mt-3">
+                        <Link to={"bookings/" + room.id} className="btn btn-hotel btn-sm">View / Book</Link>
+                    </div>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+} 
