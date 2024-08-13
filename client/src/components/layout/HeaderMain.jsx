1f99a75a77628039eb31fc4d1c9250025769adf2
Mint Rosetta <150432237+mintrosetta@users.noreply.github.com>
2024-08-13 20:17:05 +0700

    Create HeaderMain.jsx

---
+++ client/src/components/layout/HeaderMain.jsx
@@ -0,0 +1,11 @@
+export default function HeaderMain() {
+    return (
+        <header className="header-banner">
+            <div className="overlay"></div>
+            <div className="animated-texts overlay-content">
+                <h1>Welcome to <span className="hotel-color">lakeSide Hotel</span></h1>
+                <h4>Experience the Best Hospotality in Town</h4>
+            </div>
+        </header>
+    )
+}
